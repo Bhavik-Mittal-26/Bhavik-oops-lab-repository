357a131a63f2a88c2dbf15ec78123c1c8b575502
Bhavik-Mittal-26 <26bmittal@gmail.com>
2024-11-18 21:30:08 -0800

    Update exp10.cpp

--- exp10.cpp
+++ exp10.cpp
@@ -1,23 +1,42 @@
-#include <iostream>
+#include<iostream>
+#include<exception>
+
 using namespace std;
-
-class Animal {
-public:
-    void sound() {
-        cout << "The animal makes a sound." << endl;
+class InsufficientFundsException:public exception{
+    public:
+    const char* what() const noexcept override{
+        return "Error: Insufficient funds for this withdrawal.";
     }
 };
-
-class Dog : public Animal {
+class BankAccount{
+    double balance;
 public:
-    void sound() {
-        cout << "The dog barks." << endl;
+    BankAccount(double initialBalance = 0.0) : balance(initialBalance){}
+    void deposit(double amount){
+        balance +=amount;
+        cout<< "Deposited : $"<< amount<<", New Balance: $"<< balance<<endl;
+    }
+    void  withdraw(double amount){
+        if(amount > balance){
+            throw InsufficientFundsException();
+        }
+        balance -=amount;
+        cout<< "Withdraw: $"<< amount<<", New Balance: $"<< balance << endl;
+    }
+    double accountBalance() const{
+        return balance;
     }
 };
-
-int main() {
-    Dog myDog;
-    myDog.sound();
-
+int main(){
+    BankAccount account(100.0);
+    try{
+        account.deposit(50);
+        account.withdraw(30);
+        account.withdraw(300);
+    }
+    catch(const InsufficientFundsException& e){
+        cerr<<e.what()<<endl;
+    }
+    cout<<"Final balance : " << account.accountBalance()<<endl;
     return 0;
 }
