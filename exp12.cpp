12df1c201f1307c721dd4e7ab9246f624706c8a9
Bhavik-Mittal-26 <26bmittal@gmail.com>
2024-11-18 21:32:42 -0800

    Update exp12.cpp

--- exp12.cpp
+++ exp12.cpp
@@ -1,39 +1,44 @@
 #include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
 using namespace std;
-
-class Contact {
-    string name;
-    string phoneNumber;
-
-public:
-    // Default constructor
-    Contact() {
-        name = "Unknown";
-        phoneNumber = "000-000-0000";
+bool isWordPresent(string sentence, string word)
+{
+    // To break the sentence in words
+    stringstream s(sentence);
+ 
+    // To temporarily store each individual word
+    string temp;
+ 
+    while (s >> temp) {
+ 
+        // Comparing the current word
+        // with the word to be searched
+        if (temp.compare(word) == 0) {
+            return true;
+        }
     }
+    return false;
+}
+int main()
+{
+   ofstream o1("logfile.txt");
+   o1<<"[2023-12-1 10:00:00] Application started"<<endl;
+   o1<<"[2023-12-1 10:00:05] Error : Connection Failed"<<endl;
+   o1<<"[2023-12-1 10:10:00] Error : Database Error"<<endl;
+   o1<<"[2023-12-1 10:15:00] Warning: Close the Application"<<endl;
 
-    // Parameterized constructor
-    Contact(string contactName, string contactNumber) {
-        name = contactName;
-        phoneNumber = contactNumber;
+   ofstream e1("error.txt");
+
+    string line;
+    string word = "Error";
+   ifstream in("logfile.txt");
+   while(in){
+    getline(in,line);
+    if(isWordPresent(line,word)==true){
+        e1<<line<<endl;
     }
-
-    // Display contact details
-    void display() const {
-        cout << "Contact Name: " << name << endl;
-        cout << "Phone Number: " << phoneNumber << endl;
-        cout << endl;
-    }
-};
-
-int main() {
-    Contact contact1("John Doe", "123-456-7890");
-    cout << "Contact 1 Details:" << endl;
-    contact1.display();
-
-    Contact contact2;
-    cout << "Contact 2 Details (Default Constructor):" << endl;
-    contact2.display();
-
-    return 0;
+    cout<<line<<endl;
+   }
+   return 0;
 }
